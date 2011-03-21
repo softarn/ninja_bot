93c354205a01539f5e28f133b551de3341199f73
Marcus Höjvall <softarn@gmail.com>
2011-03-21 22:23:01 +0100

    avail_search now works for all directions

--- apps/reversi/src/board.erl
+++ apps/reversi/src/board.erl
@@ -13,26 +13,48 @@ game_state(#game{board = {Black, White}}) ->
 
 avail_moves(#game{board = {Black, White}}, Who) ->
     case Who of
-	0 -> avail_search(Black, White);
-	1 -> avail_search(White, Black)
+	0 -> avail_init(Black, White);
+	1 -> avail_init(White, Black)
     end.
 
-avail_search(Player, Other) ->
+
+
+avail_init(Player, Other) ->
     io:format("players:~w~n", [(Player bor Other)]),
     Empty = (pow(2,64) - 1) bxor (Player bor Other),
     io:format("Empty:~w~n", [Empty]),
-    NewPla = Player bsr 8,
+    EmptyR = Empty bxor 9259542123273814144,	    %Delete the right side of empty so bits dont wrap
+    EmptyL = Empty bxor 72340172838076673,	    %Delete the left side of empty so bits dont wrap
+    avail_start(Player, Other, Empty, fun(X) -> X bsr 8 end) bor    %North search
+    avail_start(Player, Other, EmptyL, fun(X) -> X bsr 7 end) bor   %Norteast search
+    avail_start(Player, Other, EmptyL, fun(X) -> X bsl 1 end) bor   %East search
+    avail_start(Player, Other, EmptyL, fun(X) -> X bsl 9 end) bor   %Southeast search
+    avail_start(Player, Other, Empty, fun(X) -> X bsl 8 end) bor    %South search
+    avail_start(Player, Other, EmptyR, fun(X) -> X bsl 7 end) bor   %Southwest search
+    avail_start(Player, Other, EmptyR, fun(X) -> X bsr 1 end) bor   %West search
+    avail_start(Player, Other, EmptyR, fun(X) -> X bsr 9 end).	    %Northwest search
+
+avail_start(Player, Other, Empty, Bitfun) ->
+    NewPla = Bitfun(Player),
     Potential = (NewPla band Other),
     io:format("Pot:~w~n", [Potential]),
-    avail_search(Other, Potential, 0, Empty).
+    avail_search(Other, Potential, 0, Empty, Bitfun).
 
-avail_search(_, 0, Possible, _) -> Possible;
-avail_search(Other, Potential, Possible, Empty) ->
+avail_search(_, 0, Possible, _, _) -> Possible;
+avail_search(Other, Potential, Possible, Empty, Bitfun) ->
     io:format("Pot:~w~n", [Potential]),
-    NewPot = (Potential bsr 8) band Other,
-    NewPos = Possible bor ((Potential bsr 8) band Empty),
+    NewPot = Bitfun(Potential) band Other,
+    NewPos = Possible bor (Bitfun(Potential) band Empty),
     io:format("Pos:~w~n", [Possible]),
-    avail_search(Other, NewPot, NewPos, Empty).
+    avail_search(Other, NewPot, NewPos, Empty, Bitfun).
+
+avail_search_s(_, 0, Possible, _) -> Possible;
+avail_search_s( Other, Potential, Possible, Empty) ->
+    io:format("Pot:~w~n", [Potential]),
+    NewPot = (Potential bsl 8) band Other,
+    NewPos = Possible bor ((Potential bsl 8) band Empty),
+    io:format("Pos:~w~n", [Possible]),
+    avail_search_s(Other, NewPot, NewPos, Empty).
 
 %From klarnas reversi.erl
 count_ones(I) ->
